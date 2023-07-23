7057d1c034333d3cbd4ef91e7dcc0eb48b085b5c
Asif Nishat <asifnishat@Asifs-MacBook-Air.local>
2023-07-23 12:34:04 +0530

    first commit

---
+++ src/app/buy-token/buy-token.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-token',
+  templateUrl: './buy-token.component.html',
+  styleUrls: ['./buy-token.component.css']
+})
+export class BuyTokenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
